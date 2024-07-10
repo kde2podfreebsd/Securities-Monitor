4267a24e7283f6469dcc20fa77be9c872e95117a
Bogomolov Ivan <qhomop@gmail.com>
2024-07-10 13:24:47 +0300

    ojwdfhlsdf

--- src/bot/handlers/alerts.py
+++ src/bot/handlers/alerts.py
@@ -78,3 +78,11 @@ async def send_plots(files: list, market):
         
         await bot.send_media_group(os.getenv("TELEGRAM_GROUP_CHATID"), media_group)
 
+
+@bot.message_handler(commands=['info'])
+async def get_chat_info(message):
+    chat_id = message.chat.id
+    chat_type = message.chat.type
+    chat_title = message.chat.title
+    await bot.send_message(message.chat.id, f'ID чата: {chat_id}\nТип чата: {chat_type}\nНазвание чата: {chat_title}')
+
